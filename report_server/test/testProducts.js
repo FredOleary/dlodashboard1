f760a58681a3d694d39d45b060b7f2b60b750360
fredoleary <fred@essentel.com>
2018-07-19 09:02:16 -0700

    Travis CI testing Added logging message

--- report_server/test/testProducts.js
+++ report_server/test/testProducts.js
@@ -30,9 +30,9 @@ describe('Testing Products API', function () {
 					.end(function(err, res) {
 						res.should.have.status(200);
 						expect(res.body.products).to.be.an('array');
-						console.log("Number of products: " +res.body.products.length);
 						expect(res.body.products.length).to.be.equal(2);
 						expect(res.body.products[0].description).to.be.equal("Product-1");
+						console.log("Date SubString: " +res.body.products[0].updatedDate.substring(0, ("2018-01-01T").length));
 						expect(res.body.products[0].updatedDate).to.be.equal("2018-01-01T08:00:00.000Z");
 						done(err);
 					});
